29252d5e12a99e5ff73b0984f689316403ac3772
lizandreina <156568298+lizandreina@users.noreply.github.com>
2024-06-17 15:42:25 +0200

    Create 16 columns and 16 rows

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-const container = document.querySelector('.container');
+const containerColumn = document.querySelector('.container');
 
-function addDivs(columns){
-    for (let i = 1; i < columns ; i++){
-        const col = document.createElement('div');
-        col.classList.add('col');
-        container.appendChild(col);
+function addDivs(rows, columns){
+    for (let i = 0; i < rows ; i++){
+    const row = document.createElement('div');
+    row.classList.add('row');
+        for (let j = 0; j < columns ; j++){
+            const col = document.createElement('div');
+            col.classList.add('col');
+            row.appendChild(col);
+        }
+    containerColumn.appendChild(row);
     }
 }
 
-addDivs(17);
+addDivs(16, 16);
