42f26ba637b1be4bfac12d8baa8b6939603d1548
lizandreina <156568298+lizandreina@users.noreply.github.com>
2024-06-18 17:23:02 +0200

    Create new grid with the input number of squares

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const container = document.querySelector('.container');
+let container = document.querySelector('.container');
 
 function addSquares(number){
     for (let i = 0; i < number ; i++){
@@ -20,8 +20,13 @@ addSquares(16);
 const button = document.querySelector('button');
 
 function getSquares(){
-    let number = prompt('How many squares do you want per side?', 'Max. 100');
+    let choice = parseInt(prompt('How many squares do you want per side?', 'Max. 100'));
     container.remove();
+
+    container = document.createElement('div');
+    container.classList.add('container');
+    document.body.appendChild(container);
+    addSquares(choice);
 } 
 
 button.addEventListener('click', getSquares);
