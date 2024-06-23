3dd76b9be9a69b5416faa1d1ad704c01fcc78723
lizandreina <156568298+lizandreina@users.noreply.github.com>
2024-06-23 14:02:47 +0200

    Refactor JS code

--- script.js
+++ script.js
@@ -72,6 +72,15 @@ firstGrid;
 
 const button = document.querySelector('button');
 
+function addNewGrid(choice){
+    container.remove();
+    
+    container = document.createElement('div');
+    container.classList.add('container');
+    document.body.appendChild(container);
+    addSquares(choice);
+}
+
 function getSquares(){
     let choice = prompt('How many squares would you like on each side?', 'Max. 100');
 
@@ -81,21 +90,11 @@ function getSquares(){
         if (isNaN(choice) || choice > 100){
             choice = prompt('Please choose a number below 101');
             if (choice < 101 && choice > 1) {
-                container.remove();
-    
-                container = document.createElement('div');
-                container.classList.add('container');
-                document.body.appendChild(container);
-                addSquares(choice);
+                addNewGrid(choice);
             } else if (choice < 2 && choice !== null) {
                 choice = prompt('Please choose a number higher than 1');
                 if (choice < 100 && choice > 1) {
-                    container.remove();
-        
-                    container = document.createElement('div');
-                    container.classList.add('container');
-                    document.body.appendChild(container);
-                    addSquares(choice);
+                    addNewGrid(choice);
                 }
             }
         } else if (choice < 2) {
@@ -103,28 +102,13 @@ function getSquares(){
             if (choice > 100) {
                 choice = prompt('Please choose a number below 101');
                 if (choice < 101 && choice > 1) {
-                    container.remove();
-        
-                    container = document.createElement('div');
-                    container.classList.add('container');
-                    document.body.appendChild(container);
-                    addSquares(choice);
+                    addNewGrid(choice);
                 }
             } else if (choice < 101 && choice > 1) {
-                container.remove();
-    
-                container = document.createElement('div');
-                container.classList.add('container');
-                document.body.appendChild(container);
-                addSquares(choice);
+                addNewGrid(choice);
             }
         } else {
-            container.remove();
-
-            container = document.createElement('div');
-            container.classList.add('container');
-            document.body.appendChild(container);
-            addSquares(choice);
+            addNewGrid(choice);
         }
     }
 }
